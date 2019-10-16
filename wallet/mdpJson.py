ebfea8fdc67ae695098183a024dbd800ca9d8fe3
Desprez Thibault <thibault.desprez1@gmail.com>
2019-10-16 12:58:21 +0200

    passage a des fonctions et test

--- wallet/mdpJson.py
+++ wallet/mdpJson.py
@@ -1,12 +1,34 @@
 import json
 
-with open("test.json", "r+") as f:
-    content = f.read()
-    jload = json.loads(content)
-    jload["entries"].append({"username":"lui", "nonce" :"dbd4a82efcc7950b2d6ab1abafc0453b", "passwd" : "05fe5e"})
 
-    f.seek(0)
-    f.truncate(0)
-    f.write(json.dumps(jload, indent=4).replace("    ",'\t'))    
+def addEntry(file, application, username, nonce, passwd):
+    with open(file, "r+") as f:
+        content = f.read()
+        jload = json.loads(content)
+        jload["entries"].append({"application" : application,"username":username, "nonce" :nonce, "passwd" : passwd})
+        f.seek(0)
+        f.truncate(0)
+        f.write(json.dumps(jload, indent=4).replace("    ",'\t'))    
+
+
+def getEntries(file):
+    with open(file, "r") as f:
+        content = f.read()
+        jload = json.loads(content)
+        return jload["entries"]
+
+def getEntry(file, application):
+    with open(file, "r") as f:
+        content = f.read()
+        jload = json.loads(content)
+        return [d for d in jload["entries"] if d["application"] == application ]
+
+
+def genNewFile(filename, username, mainPasswdHash):
+    with open(filename, "x") as f:
+        jload = {"mainuser": username, 	"mainpasswd": mainPasswdHash, "entries": [] }
+        f.write(json.dumps(jload, indent=4).replace("    ",'\t'))
+
+
 
 
