238316de509921765388dbe68e5b5550e5cba688
Desprez Thibault <thibault.desprez1@gmail.com>
2019-10-16 10:22:18 +0200

    patch du nonce

--- wallet/mdpCypher.py
+++ wallet/mdpCypher.py
@@ -9,29 +9,24 @@ from cryptography.hazmat.primitives.ciphers.modes import CTR
 from cryptography.hazmat.backends import default_backend
 
 
-
-# Generation du salt a la sauvegarde du wallet
-# et sauvegarde au début du fichier
-nonce = b'\x9a\xbd\x1e\x9f\x05\xde\xe5\xc9\xac\x82\xfa5@*\xb9\xd3'
-
-
 # mainpasswd = str of the userpasswd
 # passToCrypt = str to crypt
 # return = byte string  
 def mdpCrypt(mainpasswd, passToCrypt):
+    nonce = os.urandom(16)
     key = bcrypt.kdf( password=bytes(mainpasswd, "utf8"), salt=nonce, desired_key_bytes=32, rounds=100)
     Ciph = Cipher(AES(key), CTR(nonce), default_backend())
     Encryptor = Ciph.encryptor()
     
-    return Encryptor.update(bytes(passToCrypt, "utf8")).hex()
+    return Encryptor.update(bytes(passToCrypt, "utf8")).hex() , nonce.hex()
   
 
 # mainpasswd = str of the userpasswd
 # passToDecrypt = byte string
 # return String 
-def mdpDecrypt(mainpasswd, passToDecrypt):
-    key = bcrypt.kdf( password=bytes(mainpasswd, "utf8"), salt=nonce, desired_key_bytes=32, rounds=100)
-    Ciph = Cipher(AES(key), CTR(nonce), default_backend())
+def mdpDecrypt(mainpasswd, passToDecrypt, nonce):
+    key = bcrypt.kdf( password=bytes(mainpasswd, "utf8"), salt=bytes.fromhex(nonce), desired_key_bytes=32, rounds=100)
+    Ciph = Cipher(AES(key), CTR(bytes.fromhex(nonce)), default_backend())
     Decryptor = Ciph.decryptor()
 
     return Decryptor.update(bytes.fromhex(passToDecrypt)).decode("utf8")
@@ -44,4 +39,5 @@ if __name__ == "__main__":
         print(mdpCrypt(mp, uc))
     else:
         c = input("the thing you want to decrypt : ")
-        print(mdpDecrypt(mp, c))
+        n = input("the associate nonce : ")
+        print(mdpDecrypt(mp, c,n ))
