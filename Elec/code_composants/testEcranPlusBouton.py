9a2849726df7f36de70a18e599bfa30515670ff8
kaetir <thibault.desprez@isen.yncrea.fr>
2019-10-16 15:54:32 +0200

    ajout jusqu'a 5 boutons

--- Elec/code_composants/testEcranPlusBouton.py
+++ Elec/code_composants/testEcranPlusBouton.py
@@ -12,8 +12,11 @@ from PIL import ImageFont
 os.system("sudo rmmod spi_bcm2835 && sudo modprobe spi_bcm2835")
 time.sleep(2)
 
-pinBtn=17
-pinBtn2=18
+pinBtn=18
+pinBtn2=17
+pinBtn3=27
+pinBtn4=22
+pinBtn5=20
 
        
 disp = Adafruit_SSD1306.SSD1306_128_64(rst=24, dc=23, spi=SPI.SpiDev(0, 0, max_speed_hz=8000000))
@@ -38,12 +41,18 @@ GPIO.setmode(GPIO.BCM)
 #GPIO.cleanup()
 GPIO.setup(pinBtn , GPIO.IN)
 GPIO.setup(pinBtn2, GPIO.IN)
+GPIO.setup(pinBtn3 , GPIO.IN)
+GPIO.setup(pinBtn4, GPIO.IN)
+GPIO.setup(pinBtn5, GPIO.IN)
 
 while(True):
     etat=GPIO.input(pinBtn)
     etat2=GPIO.input(pinBtn2)
-    if(etat==1 or etat2==1):
-        print("Appui detecte : " + str(etat) + " : " + str(etat2)  )
+    etat3=GPIO.input(pinBtn3)
+    etat4=GPIO.input(pinBtn4)
+    etat5=GPIO.input(pinBtn5)
+    if(etat==1 or etat2==1 or etat3==1 or etat4==1 or etat5==1):
+        print("Appui detecte : " + str(etat) + " : " + str(etat2) +" : " + str(etat3) + " : " + str(etat4)+ " : " + str(etat5))
         disp.clear()
         draw = ImageDraw.Draw(image)
         draw.rectangle((0,0,disp.width-1,disp.height-1), outline=1, fill=0)
@@ -54,10 +63,13 @@ while(True):
 
 
     #print(etat)
-    while(etat == 1 or etat2==1):
+    while(etat == 1 or etat2==1 or etat3==1 or etat4==1 or etat5==1):
         time.sleep(0.1)
         etat=GPIO.input(pinBtn)
         etat2=GPIO.input(pinBtn2)
+        etat3=GPIO.input(pinBtn3)
+        etat4=GPIO.input(pinBtn4)
+        etat5=GPIO.input(pinBtn5)
 
 
 
