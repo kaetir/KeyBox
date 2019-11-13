928ec3de673335368064712670519c1afdce8ffc
kaetir <thibault.desprez@isen.yncrea.fr>
2019-11-13 09:53:52 +0100

    fusion des fichiers

--- hotspot/mod_hotspot.py
+++ hotspot/mod_hotspot.py
@@ -1,18 +1,37 @@
-from os import system
+#!/usr/bin/python3
 
-def new_passwd_hotspot():
-    system("python3 edit_hostapd_conf.py")
+from os import system
+from genRngPasswd import randomString
+
+def change_wifi_passwd():
+    dic = {}
+
+    with open("/etc/hostapd/hostapd.conf","r+") as f:
+        content = f.read()[:-1]
+        data = content.split("\n")
+        dic = {x.split("=")[0] : x.split("=")[-1] for x in data}   
+
+    with open("/etc/hostapd/hostapd.conf","w") as f:
+        dic["wpa_passphrase"] = randomString(8)
+        tamerelapute = [ "{}={}\n".format(key,dic[key]) for key in dic]
+        print(tamerelapute)
+        for l in tamerelapute:
+            f.writelines(l)
+
 
 def en_hostspot():
-    new_passwd_hotspot()
+    change_wifi_passwd())
     system("systemctl start hostapd")
+
 
 def dis_hostspot():
     system("systemctl stop hostapd")
+
 
 def get_wifi_passwd():
     with open("/etc/hostapd/hostapd.conf","r+") as f:
         content = f.read()
         data = content.split("\n")[:-1]
         dic = {x.split("=")[0] : x.split("=")[-1] for x in data}   
-        return dic["wpa_passphrase"] 
+        return dic["wpa_passphrase"]
+
