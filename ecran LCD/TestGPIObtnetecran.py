cc70e94f7f318c27a724b0473062e7b173d0d9d4
kaetir <thibault.desprez@isen.yncrea.fr>
2019-11-15 14:05:33 +0100

    ajout de la lecture par un fichier txt

--- ecran LCD/TestGPIObtnetecran.py
+++ ecran LCD/TestGPIObtnetecran.py
@@ -19,6 +19,10 @@ pinBtn=[16,19,20,21,26]
 
 GPIO.setmode(GPIO.BCM)
 
+#boutons
+for i in pinBtn:
+    GPIO.setup(pinBtn, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
 disp = Adafruit_SSD1306.SSD1306_128_64(rst=24, dc=23, spi=SPI.SpiDev(0, 0, max_speed_hz=8000000))
 font = ImageFont.load_default()
 
@@ -42,83 +46,84 @@ def draw_text(text, ligne, col = 0):
     draw.text((colones[col]+2,lignes[ligne]),text, font=font, fill=255)
 
 def clear_scr():
-    draw = ImageDraw.Draw(image)
+    global image
+    global draw
+    image = Image.new('1', (disp.width, disp.height))
+    draw= ImageDraw.Draw(image) 
     disp.clear()
     disp.display()
 
- 
 def Menu():
-    draw_text("Menu", 0)
-    draw_text("1. G-mail", 1)
-    draw_text("2. Facebook", 2)
-    draw_text("3. Twitter", 3)
-    draw_text("pornhub.com", 4, 3)
-        
+    f= open("fichier.txt","r")
+    content = f.read()
+    data = content.split("\n")
+    draw_text(data[0],0)
+    for i in range(len(data)-1):
+        draw_text(str(i)+". "+ data[i+1],i+1)
+    
+    
 def curs(x,y):
     # x et y sont en colones lignes
     draw.rectangle((colones[x],lignes[y],colones[x]+6,lignes[y]+8), outline=1, fill=255)
     disp.image(image)
     disp.display()
 
-#Menu()
+Menu()
+
+
 disp.image(image)
 disp.display()
 
 clear_scr()
 
-for x in range(len(string.ascii_letters)):
-    draw_text(string.ascii_letters[x],x//nbCols,x%nbCols)
+#alphabet
+"""for x in range(len(string.ascii_letters)):
+    draw_text(string.ascii_letters[x],x//nbCols,x%nbCols)"""
     
-disp.image(image)
-disp.display()
 
-curs(1, 0)
 
+
+#curs(1,0)
 counter = 0
-x=0
-
-for i in pinBtn:
-    GPIO.setup(pinBtn, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-
-"""
-while(True):
-    etat= [GPIO.input(i) for i in pinBtn]
-
-    if(False in etat):
-        print("Appui detecte : "+ str(etat))
-        disp.clear()
-        draw = ImageDraw.Draw(image)
-        draw.rectangle((0,0,disp.wiBtn_Lefth-1,disp.height-1), outline=1, fill=0)
-        draw.text((8,15),'ID: ANTOINE', font=font, fill=255)
-        draw.text((8,25),'Password: ********', font=font, fill=255)
-        
-        disp.image(image)
-        disp.display()"""
-
-
 
 while True:
+    asModif = False
     BtnRight= [GPIO.input(pinBtn[4])] #droite
     BtnLeft= [GPIO.input(pinBtn[1])] #gauche
     BtnUp=[GPIO.input(pinBtn[2])]
     BtnDown=[GPIO.input(pinBtn[3])]
+    BtnOk= [GPIO.input(pinBtn[0])]
+    
     if (False in BtnRight ):
         counter += 1
-        time.sleep(0.2)
-        print(counter)
+        asModif = True
         
-    elif (False in BtnLeft):
+    if (False in BtnLeft):
         counter -= 1
-        time.sleep(0.2)
-        print (counter)
+        asModif = True
         
-    elif (False in BtnUp):
+    if (False in BtnUp):
         counter += 21
-        time.sleep(0.2)
-        print(counter)
+        asModif = True
         
-    elif (False in BtnDown):
+    if (False in BtnDown):
         counter -= 21
+        asModif = True
+        
+    #if (False in BtnOk):
+        
+    if asModif:
+        clear_scr()
+        Menu()
+        draw_text(str(counter),4)
+        disp.image(image)
+        disp.display()
         time.sleep(0.2)
-        print(counter)
+        
+        
+        
+
+         
+disp.image(image)
+disp.display()
+selectMenu()
