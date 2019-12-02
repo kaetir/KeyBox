d40f5d6b49fb251b97336c4808d14ddeabca835c
kaetir <thibault.desprez@isen.yncrea.fr>
2019-12-02 16:00:20 +0100

    déplacement de la fonction dico dans le genpasswd
    et réencodage du dico en utf8

--- wallet/genRngPasswd.py
+++ wallet/genRngPasswd.py
@@ -1,19 +1,27 @@
-
 import string
 import random
 
-def randomString(stringLength=10):
+
+def random_string(string_length: int = 10) -> str:
+    """
+    @summary genere un mot de passe aléatoire avec minuscules majuscules et ponctuation
+    @param string_length: int -> nombre de lettres
+    @return str: la phrase de sortie
+    """
     letters = string.ascii_letters + string.punctuation
-    return ''.join(random.choice(letters) for i in range(stringLength))
+    return ''.join(random.choice(letters) for i in range(string_length))
+
+
+def gen_phrase(nb_mot: int = 10) -> str:
+    """ 
+    @summary genere une phrase aléatoire a partir d'un dico
+    @param nb_mot: int ->  nbMot
+    @return str: la phrase de sortie
+    """
+    with open('liste_francais.txt', 'r') as f:
+        return " ".join(random.choices(f.read().split('\n'), k=nb_mot))
 
 
 if __name__ == "__main__":
-    print(randomString(10))
-
-
-
-
-
-
-
-
+    print(random_string(10))
+    print(gen_phrase())
