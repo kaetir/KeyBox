1751a378ccae71099be6a6f9fba3a629bf0c6289
kaetir <thibault.desprez@isen.yncrea.fr>
2019-12-03 11:49:32 +0100

    ajoute des tests de mapping boutons

--- ecran OLED/menu.py
+++ ecran OLED/menu.py
@@ -4,18 +4,35 @@ from ecran import Ecran
 
 class Menu():
 
-    def __init__(self, ecran, boutons) -> None:
+    def __init__(self, ecran: Ecran, boutons: Boutons, fichier: str ="fichier.txt") -> None:
+        """
+        @summary constructeur
+        @param ecran: Ecran -> réference sur l'écran
+        @param boutons: Boutons -> références sur l'objets boutons$
+        @param fichier: str -> fichier avec les entrés du menu
+        """
         super().__init__()
         self.ecran = ecran
         self.boutons = boutons
-
-    def print(self):
-        f = open("fichier.txt", "r")
+        f = open(fichier, "r")
         content = f.read()
         data = content.split("\n")
-        self.ecran.draw_text(data[0], 0)
-        for i in range(len(data) - 1):
-            self.ecran.draw_text(str(i) + ". " + data[i + 1], i + 1)
+        self.title = data[0]
+        self.entries = data[1:]
 
-
+    def print(self) -> None:
+        """
+        @summary affiche le menu
+        """
+        self.ecran.draw_text(self.title, 0)
+        for i in range(len(self.entries)):
+            self.ecran.draw_text(str(i+1) + ". " + self.entries[i], i + 1)
+    
+    
+    def select(self) -> int:
+        """
+        @summary lit l'état des boutons jusqu'a ce que OK ou BACK soit préssé
+        @return  -1 si BACK
+        @return  numéro de l'entré si OK 
+        """
 
